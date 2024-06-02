4447bce014a6105d17cda434a370f509a4841ea4
Sangyeob <x1111101101@gmail.com>
2024-06-03 01:39:20 +0900

    Component 쪼개기

--- src/index.js
+++ src/index.js
@@ -8,12 +8,47 @@ const root = ReactDOM.createRoot(
   function tick() {
     const element = (
       <div>
-        <Welcome/>
+        <Welcome name="SLSLSL"/>
         <h1>Hello, world!</h1>
         <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        <Comment/>
       </div>
     );
     root.render(element);
+  }
+
+  function Comment(props) {
+    return (
+      <div className="Comment">
+        <UserInfo user={props.author}/>
+        <div className="Comment-text">
+          {props.text}
+        </div>
+        <div className="Comment-date">
+          {formatDate(props.date)}
+        </div>
+      </div>
+    );
+  }
+
+  function UserInfo(props) {
+    return (
+      <div className="UserInfo">
+        <Avatar user={props.user} />
+        <div className="UserInfo-name">
+          {props.user.name}
+        </div>
+      </div>
+    );
+  }
+
+  function Avatar(props) {
+    return (
+      <img className="Avatar"
+        src={props.user.avatarUrl}
+        alt={props.user.name}
+      />
+    );
   }
   
   class Welcome extends React.Component {
