6d4b8bcf0332376717122fc70300f906e749a2fe
Sangyeob <x1111101101@gmail.com>
2024-06-04 00:56:40 +0900

    합성 vs 상속

--- src/index.js
+++ src/index.js
@@ -1,99 +1,69 @@
 import ReactDOM from "react-dom";
 import React, {useState} from "react";
 
-function BoilingVerdict(props) {
-  if (props.celsius >= 100) {
-    return <p>The water would boil.</p>;
-  }
-  return <p>The water would not boil.</p>;
+function Dialog(props) {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        {props.title}
+      </h1>
+      <p className="Dialog-message">
+        {props.message}
+      </p>
+    </FancyBorder>
+  );
 }
 
-const scaleNames = {
-  c: 'Celsius',
-  f: 'Fahrenheit'
-};
-
-function toCelsius(fahrenheit) {
-  return (fahrenheit - 32) * 5 / 9;
-}
-
-function toFahrenheit(celsius) {
-  return (celsius * 9 / 5) + 32;
-}
-
-function tryConvert(temperature) {
-  const input = temperature
-  if (Number.isNaN(input)) {
-    return '';
-  }
-  const rounded = Math.round(input * 1000) / 1000;
-  return rounded.toString();
-}
-
-class TemperatureInput extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  handleChange = (e)=> {
-    // Before: this.setState({temperature: e.target.value});
-    console.log("HANDLE CHANGE: " + JSON.stringify(this.props))
-    this.props.onTemperatureChange(parseFloat(e.target.value));
-  }
-
-  render() {
-    const temperature = this.props.temperature;
-    const scale = this.props.scale;
-    return (
-      <fieldset>
-        <legend>Enter temperature in {scaleNames[scale]}:</legend>
-        <input value={temperature}
-               onChange={this.handleChange} />
-      </fieldset>
-    );
-  }
-}
-
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {celsius: '1.5'};
-  }
-
-  onChangeByFahrenheit = (f)=> {
-    this.setState({celsius: toCelsius(f)})
-  }
-
-  onChangeByCelsius = (c) => {
-    this.setState({celsius: c})
-  }
-
-  render() {
-    let celsius = this.state.celsius
-    return (
-      <div>
-
-        <TemperatureInput 
-            temperature={tryConvert(celsius)} 
-            scale="c" 
-            onTemperatureChange={this.onChangeByCelsius}
-        />
-
-        <TemperatureInput 
-            temperature={tryConvert(toFahrenheit(celsius))} 
-            scale="f" 
-            onTemperatureChange={this.onChangeByFahrenheit}
-        />
-
-        <BoilingVerdict celsius={celsius}/>
+/*
+function SplitPane(props) {
+  return (
+    <div className="SplitPane">
+      <div className="SplitPane-left">
+        {props.left}
       </div>
-    );
-  }
+      <div className="SplitPane-right">
+        {props.right}
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <SplitPane
+      left={
+        <Contacts />
+      }
+      right={
+        <Chat />
+      } />
+  );
+}
+*/
+
+function FancyBorder(props) {
+  return (
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
+    </div>
+  );
+}
+function WelcomeDialog() {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        Welcome
+      </h1>
+      <p className="Dialog-message">
+        Thank you for visiting our spacecraft!
+      </p>
+    </FancyBorder>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <Calculator/><hr></hr>
+    <WelcomeDialog/><hr></hr>
   </div>
 );
