b080b6385722bce566db72211d71ddbb1aa518cc
Sangyeob <x1111101101@gmail.com>
2024-06-03 22:57:57 +0900

    다중 input 제어

--- src/index.js
+++ src/index.js
@@ -96,11 +96,64 @@ class FlavorForm extends React.Component {
   }
 }
 
+class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit = (event)=> {
+    event.preventDefault()
+    alert(JSON.stringify(this.state))
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Is going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+        <input type="submit" value="제출"/>
+      </form>
+    );
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <NameForm/>
-    <EssayForm/>
-    <FlavorForm/>
+    <NameForm/><hr></hr>
+    <EssayForm/><hr></hr>
+    <FlavorForm/><hr></hr>
+    <Reservation/><hr></hr>
   </div>
 );
